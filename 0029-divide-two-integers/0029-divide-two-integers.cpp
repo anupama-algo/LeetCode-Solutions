209c9e2f7f7f9bde06c4cac9596c36792b657dea
ANUPAMA SETHI <anupama.algo.dev@gmail.com>
2026-07-07 23:55:02 +0530

    Time: 0 ms (100%), Space: 8.6 MB (72.63%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+        
+        long long a = abs((long long)dividend);
+        long long b = abs((long long)divisor);
+        long long res = 0;
+        
+        while (a >= b) {
+            long long temp = b, multiple = 1;
+            while (a >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            a -= temp;
+            res += multiple;
+        }
+        
+        return (dividend > 0) == (divisor > 0) ? res : -res;
+    }
+};
