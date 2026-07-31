3e58fb608939491395cb6a9ebcdd16aa645c9e4e
ANUPAMA SETHI <anupama.algo.dev@gmail.com>
2026-07-31 22:48:21 +0530

    Time: 0 ms (100%), Space: 20.7 MB (28.88%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int jump(std::vector<int>& nums) {
+        int jumps = 0;
+        int current_end = 0;
+        int farthest = 0;
+        
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            farthest = std::max(farthest, i + nums[i]);
+            if (i == current_end) {
+                jumps++;                 
+                current_end = farthest;  
+                if (current_end >= nums.size() - 1) {
+                    break;
+                }
+            }
+        }
+        
+        return jumps;
+    }
+};
