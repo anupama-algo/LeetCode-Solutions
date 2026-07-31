5ebd60ff37a15fabdcb3349446518c93d7af0c40
ANUPAMA SETHI <anupama.algo.dev@gmail.com>
2026-07-31 22:45:43 +0530

    Time: 0 ms (100%), Space: 9.2 MB (78.56%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    void backtrack(const string& digits, int index, string& current, 
+                   const vector<string>& mapping, vector<string>& result) {
+        if (index == digits.length()) {
+            result.push_back(current);
+            return;
+        }
+        
+        string letters = mapping[digits[index] - '0'];
+        
+        for (char c : letters) {
+            current.push_back(c);               
+            backtrack(digits, index + 1, current, mapping, result);
+            current.pop_back();                  
+        }
+    }
+
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+        
+        vector<string> mapping = {
+            "",     
+            "",     
+            "abc",  
+            "def",  
+            "ghi",  
+            "jkl",  
+            "mno",  
+            "pqrs", 
+            "tuv",  
+            "wxyz"  
+        };
+        
+        vector<string> result;
+        string current = "";
+        backtrack(digits, 0, current, mapping, result);
+        
+        return result;
+    }
+};
