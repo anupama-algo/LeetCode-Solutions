31a85a37d6eabd6c3fdaeec1e1026de945d430ef
ANUPAMA SETHI <anupama.algo.dev@gmail.com>
2026-08-28 22:54:46 +0530

    Time: 1 ms (99.99%), Space: 45.6 MB (39.36%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+
+        dfs(root, targetSum, path, result);
+
+        return result;
+    }
+
+    private void dfs(
+            TreeNode node,
+            int target,
+            List<Integer> path,
+            List<List<Integer>> result) {
+
+        if (node == null) {
+            return;
+        }
+
+        path.add(node.val);
+        target -= node.val;
+
+        if (node.left == null &&
+            node.right == null &&
+            target == 0) {
+
+            result.add(new ArrayList<>(path));
+        }
+
+        dfs(node.left, target, path, result);
+        dfs(node.right, target, path, result);
+
+        path.remove(path.size() - 1);
+    }
+}
