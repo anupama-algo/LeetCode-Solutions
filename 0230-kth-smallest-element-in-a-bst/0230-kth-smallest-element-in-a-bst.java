cffeebae82c013c07f4fcaacad66eea6dd8d812d
ANUPAMA SETHI <anupama.algo.dev@gmail.com>
2026-08-29 22:26:45 +0530

    Time: 1 ms (37.76%), Space: 46.7 MB (50.8%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (true) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            k--;
+
+            if (k == 0) {
+                return current.val;
+            }
+
+            current = current.right;
+        }
+    }
+}
