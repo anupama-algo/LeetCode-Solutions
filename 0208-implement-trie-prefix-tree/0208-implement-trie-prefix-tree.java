7f291d206b8cd2f8f8143affa09101176f8c177f
ANUPAMA SETHI <anupama.algo.dev@gmail.com>
2026-09-11 23:28:11 +0530

    Time: 35 ms (42.43%), Space: 61.9 MB (62.15%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,53 @@
+class Trie {
+
+    private static class Node {
+        Node[] children = new Node[26];
+        boolean isEnd;
+    }
+
+    private final Node root = new Node();
+
+    public Trie() {
+    }
+
+    public void insert(String word) {
+        Node current = root;
+
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+
+            if (current.children[index] == null) {
+                current.children[index] = new Node();
+            }
+
+            current = current.children[index];
+        }
+
+        current.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Node node = findNode(word);
+        return node != null && node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        return findNode(prefix) != null;
+    }
+
+    private Node findNode(String word) {
+        Node current = root;
+
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+
+            if (current.children[index] == null) {
+                return null;
+            }
+
+            current = current.children[index];
+        }
+
+        return current;
+    }
+}
