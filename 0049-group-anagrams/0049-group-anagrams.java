2930a5a121979eedc96aba10f52548e7d8c3c5e5
ANUPAMA SETHI <anupama.algo.dev@gmail.com>
2026-09-20 22:46:08 +0530

    Time: 18 ms (20.59%), Space: 49.9 MB (26.54%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            int[] count = new int[26];
+
+            for (char c : str.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder key = new StringBuilder();
+
+            for (int num : count) {
+                key.append('#').append(num);
+            }
+
+            map.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
