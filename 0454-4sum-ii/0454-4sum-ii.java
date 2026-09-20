864a1dfb031037c68d42ac18f49abb64896d92b3
ANUPAMA SETHI <anupama.algo.dev@gmail.com>
2026-09-20 22:49:22 +0530

    Time: 114 ms (48.62%), Space: 46.9 MB (70.7%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int a : nums1) {
+            for (int b : nums2) {
+                int sum = a + b;
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        int answer = 0;
+
+        for (int c : nums3) {
+            for (int d : nums4) {
+                int sum = c + d;
+                answer += map.getOrDefault(-sum, 0);
+            }
+        }
+
+        return answer;
+    }
+}
